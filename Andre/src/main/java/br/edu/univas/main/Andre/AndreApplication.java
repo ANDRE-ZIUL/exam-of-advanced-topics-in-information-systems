71528e7522d029a7567caf7376c18cc016d3a7bd
ANDRE-ZIUL <dedelcdias@gmail.com>
2024-04-16 19:17:41 -0300

    first commit of exam

---
+++ Andre/src/main/java/br/edu/univas/main/Andre/AndreApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univas.main.Andre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndreApplication.class, args);
+	}
+
+}
---
+++ Andre/src/test/java/br/edu/univas/main/Andre/AndreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.univas.main.Andre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
